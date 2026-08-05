f28955a156d0c46ecfd9d44319d7ee03cdee9484
nguyen the lam <lamx8qb@gmail.com>
2026-08-05 16:26:21 +0700

    .

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -48,6 +48,7 @@ class Settings(BaseSettings):
             "http://localhost:4173",
             "http://127.0.0.1:4173",
             "https://visual-search-engine-git-deploy-lamant1411s-projects.vercel.app",
+            "https://visual-search-engine-plum.vercel.app",
         ]
     )
 
